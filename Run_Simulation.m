f42c158ab71d1a7a3add10a77928629d1dd370ca
Al3xand3er <medranojaziel@gmail.com>
2025-11-07 23:37:55 -0800

    Update Run_Simulation.m

--- Run_Simulation.m
+++ Run_Simulation.m
@@ -11,25 +11,25 @@ function Run_Simulation(fire, params, drones)
 t = 0;
     disp("START OF FIRE");
     while t <= params.t_max
-        if fire.intensity < params.stop_condition   % simultion stops when conidition are cleared
+        if all(fire.intensity(:) < params.stop_condition)   % simultion stops when conidition are cleared
             disp("FIRE EXTINGUSHIED")
             break
         end
 % updates the fire throughout the simulation 
-fire = fire_step(fire, params)
+fire = fire_step(fire, params);
 %updates the drones thorught the simulation
     for i = 1:length(drones)
-        [drones(i), fire] = drone_update(drones(i), drones, fire, params, i)
+        [drones(i), fire] = update_drone(drones(i), fire, params, drones, i);
     end
 %creates the data regarding the visualtion of the simulation
     if mod(round(t / params.dt), 10) == 0            % AI  helped when it came to plotting every 10 steps rather every single interation
         Plot_State(fire, params, drones, t);
         drawnow;
     end
-    t = t +params.dt
+    t = t +params.dt;
+    params.t = t;
     end
     disp("Simulation complete please wait...");
 Summary_Table = Summarize_Results(drones, fire);  % gathers the data of both the fires and the drones thourght the simlaution
 Save_Results(drones, fire, params, Summary_Table); % saves data of each simulation
 end
-
