518bb2d3767a4e906a9dabf9cbedb0dab27c7f83
Al3xand3er <medranojaziel@gmail.com>
2025-11-07 20:42:19 -0800

    Update avoid_collision.m
    added the drone movement so it doesn't appear to teleport to fire

--- avoid_collision.m
+++ avoid_collision.m
@@ -20,3 +20,28 @@ minSep = 0.5;                       % Minimum safe distance in grid units, here 
         end
     end
 end
+
+function newPos = move_toward(currPos, targetPos, speed, dt, gridSize)
+
+direction  = targetPos - currPos; % Direction will be the position of target - current position
+distance = norm(direction);       % Gets the amount gotten from direction and makes it the distance
+
+    if distance == 0              % if drone distance to fire is 0, drone already at cell, keeps drone at cell
+        newPos = currPos; 
+        return;
+    end
+
+step = speed * dt;                % amount of distance drone will move slightly so it wont look like it instantly moves to cell, actually travels to cells
+
+    if step >= distance           % if distance is less than step, drone at the cell so traveling is done and sets target position into new position
+        newPos = targetPos;
+    else
+        directionUnit = direction / distance;        % otherwise drone will keep moving in target direction slighlty to give appearence of traveling
+        newPos  = currPos + directionUnit * step;
+    end
+
+
+newPos(1) = min(max(newPos(1), 1), gridSize(1));       % Keeps drones in row and column grid
+newPos(2) = min(max(newPos(2), 1), gridSize(2));
+
+end
