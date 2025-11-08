36a09797dfd2c7a87c9f7c1df3576b8092d4b00a
WilliamBarajas1 <wbar5497@giant.cos.edu>
2025-11-07 21:18:34 -0800

    Summary_Table.m

--- Summary_Table.m
+++ Summary_Table.m
@@ -9,18 +9,18 @@ function Summary_Table(drones, fire, safePos)
 %   - Tables the fire data over the course of the simluation
 
 
-numberofdrones = numel(drones);
-Summary = struct([]);
+numberofdrones = numel(drones)
+Summary = struct([])
 
     for i = 1:numberofdrones                        %number of drones we run this simulation
-Summary(i).DroneID = i;                             %"name" attached to each drone as their ID
-Summary(i).Distance = drones(i).logs.distance;       % total distance each drone traveled
-Summary(i).Fires_Extinguished = cellsExtinguished ;   % total number of fires extinguished thorughout simulation
-Summary(i).Collision_Avoidance_Counter = safePos(i);   % number of times each drone had a close encounter
-Summary(i).Final_Postion = drones(i).pos;            % final position of each drone
+Summary(i).DroneID = i                             %"name" attached to each drone as their ID
+Summary(i).Distance = drones(i).logs.distance       % total distance each drone traveled
+Summary(i).Fires_Extinguished = cellsExtinguished    % total number of fires extinguished thorughout simulation
+Summary(i).Collision_Avoidance_Counter = safePos(i)   % number of times each drone had a close encounter
+Summary(i).Final_Postion = drones(i).pos            % final position of each drone
     end
 
-Summary_Table = struct2table(Summary);
+Summary_Table = struct2table(Summary)
 disp(Summary_Table);
 
 end
